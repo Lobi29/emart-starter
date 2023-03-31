5e5f0353951e5d4794d8e41ca762ce563bec1733
chao lobi <gogoiladitya@gmail.com>
2023-03-31 15:27:00 +0530

    product details page is added

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,22 +1,3 @@
-// import React from 'react';
-// import Link from 'next/link';
-
-// import { urlFor } from '@/lib/client';
-
-// const Product = ({ product: {image, name, slug, price} }) => {
-//   return (
-//     <div>
-//       <Link href={'/product/' + slug.current}>
-//         <img src={urlFor(image && image[0])} width={250} height={250} className='product-image' />
-//         <p className='product-name'>{ name }</p>
-//         <p className='product-price'>${ price }</p>
-//       </Link>
-//     </div>
-//   )
-// }
-
-// export default Product;
-
 import React from 'react';
 import Link from 'next/link';
 
